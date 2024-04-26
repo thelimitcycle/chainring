e534199c5db476632bdc8a9b80b6564295dcb9b0
Jim <jimlakowski@gmail.com>
2024-04-25 23:47:14 -0700

    playing around with the cumulative simpson's rule. its not as easy as I thought, but I got it as a matrix multiplication:

---
+++ numerical_integration.py
@@ -0,0 +1,27 @@
+import numpy as np
+import scipy as sp 
+import finite_difference_matrix as fdm 
+import matplotlib.pyplot as plt
+
+n = 100
+Sdiy = fdm.simpsons_rule_matrix(n+1)
+
+domain = 2
+x = domain* np.arange(n+1)/n
+h = domain/n
+
+y = x**3
+Ydiy = 1.0/3 *h * Sdiy.dot(y)
+
+Y = sp.integrate.cumulative_simpson(y,x=x,initial=0)
+
+Ydiff = Ydiy-Y
+print(Sdiy)
+plt.plot(x,Y,x,Ydiy)
+plt.legend(['scipy', 'matrix mult'])
+plt.show()
+
+plt.figure()
+plt.plot(x,Ydiff)
+plt.scatter(x,Ydiff,color='red')
+plt.show()
